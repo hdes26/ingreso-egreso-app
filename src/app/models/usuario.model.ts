fccfed59e487f5e2e49e45e2e167b100ef6eed69
Hernan <desarrollo6@infinitumscitech.com>
2022-07-14 16:12:15 -0500

    en el desastre

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -1,3 +1,11 @@
 export class Usuario {
-  constructor(public uid: string , public nombre: string , public email: string ) {}
+  static fromFirebase({ email, uid, nombre }:any) {
+    return new Usuario(uid,nombre,email);
+  }
+
+  constructor(
+    public uid: string,
+    public nombre: string,
+    public email: string
+  ) {}
 }
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+})
+export class SidebarComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {}
+  logout() {
+    this.authService.logout().then( () => {
+      this.router.navigate(['/login']);
+    })
+    .catch(err => console.log(err));
+  }
+}
